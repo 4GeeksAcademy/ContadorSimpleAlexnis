de91ab9456e9b58673312d745dcce3c6902881c2
Alexnis-Herrera <alexnisherrerag@gmail.com>
2025-07-10 17:07:39 +0000

    casilisto

--- src/js/components/Contador.jsx
+++ src/js/components/Contador.jsx
@@ -3,15 +3,15 @@ import { Digito } from "./Digito"
 export const Contador = ({ counter }) => {
     return (
         <>
-            {counter}
+           
             <div className="border border-info text-center m-5 p-4 row">
 
-                <Digito digito={0} />
-                <Digito digito={0} />
-                <Digito digito={0} />
-                <Digito digito={0} />
-                <Digito digito={0} />
-                <Digito digito={9} />
+                <Digito digito/>
+                <Digito digito={counter} />
+                <Digito digito= {counter} />
+                <Digito digito= {counter} />
+                <Digito digito= {counter} />
+                <Digito digito= {counter} />
 
             </div>
         </>
